acedaa3d28aea403f5b11e3cae23eecef1de6a23
Torpeedofv <torpeedo.fv@gmail.com>
2023-08-03 21:29:11 +0100

    task 7 redo

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """A module for a type annotated function"""
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """A type annotated function that takes a string and an int
     or float as argument that returns a tuple"""
