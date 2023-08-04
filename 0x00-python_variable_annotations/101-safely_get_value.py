2940c434b7013e733b658bf1b5448c4a22e482e0
Torpeedofv <torpeedo.fv@gmail.com>
2023-08-04 08:51:45 +0100

    task 101 redo

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,6 +6,7 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)\
         -> Union[Any, T]:
+    """A type annotated function that can return either of two types"""
     if key in dct:
         return dct[key]
     else:
