ea6056ae6d1b14a7e7f6f7ba812d3f7b1933aef7
Torpeedofv <torpeedo.fv@gmail.com>
2023-08-07 20:09:03 +0100

    task 0 redo

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,9 +5,9 @@ from typing import Union
 import random
 
 
-async def wait_random(max_delay: Union[int, float]  =10) -> float:
-    """A type annotated asynchronous coroutine that takes in an integer argument
-    and returns a float"""
-    i =  random.uniform(0, max_delay)
+async def wait_random(max_delay: Union[int, float] = 10) -> float:
+    """A type annotated asynchronous coroutine that takes in an integer
+    argument and returns a float"""
+    i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
-    return i 
+    return i
