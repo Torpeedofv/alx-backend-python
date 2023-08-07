edcf1caa46f92e5025b02fe0d6de20321c3631bd
Torpeedofv <torpeedo.fv@gmail.com>
2023-08-07 20:05:01 +0100

    task 0 redo

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,7 @@ import random
 
 async def wait_random(max_delay: Union[int, float]  =10) -> float:
     """A type annotated asynchronous coroutine that takes in an integer argument
-    and returns a float"""i =  random.uniform(0, max_delay)
+    and returns a float"""
+    i =  random.uniform(0, max_delay)
     await asyncio.sleep(i)
     return i 
